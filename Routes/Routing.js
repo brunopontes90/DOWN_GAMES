c9e55c470f28d3ef1b0eb8ea219e677279d564b1
Bruno Pontes <bruno_confessor@hotmail.com>
2022-01-07 18:52:47 -0300

    Ajustes em dotenv para conexão com DB

--- Routes/Routing.js
+++ Routes/Routing.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+const env = require('dotenv').config();
 
 const express = require('express');
 const application = express();
@@ -9,10 +10,10 @@ function Router()
 {
 
     let con = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'down_games'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     });
     
     con.connect(function(err){
